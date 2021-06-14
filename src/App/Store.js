5542d73ea5b8c34deb71e4d66aa6fedced96dac0
DESKTOP-RN60I9K\Anh Minh Tran <anhminh18101997@gmail.com>
2021-06-14 17:15:48 +0700

    add .env

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,18 +1,30 @@
 import productReducer from "Features/Product/ProductSlice";
 import categoryReducer from "Features/Category/CategorySlice";
-import { configureStore } from "@reduxjs/toolkit";
-import { getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const rootReducer = {
-  product: productReducer,
-  category: categoryReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["product"],
 };
 
+const rootReducer = combineReducers({
+  product: productReducer,
+  category: categoryReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const Store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware({
     serializableCheck: false,
   }),
 });
+
+export const persistor = persistStore(Store);
 
 export default Store;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import Store from "App/Store";
+import Store, { persistor } from "App/Store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={Store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={Store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
 
